ceffe157fe469543bee71b1a59af7e7ec6ce6441
Aryan Agrawal <45911109+Aaryan00@users.noreply.github.com>
2024-11-10 21:52:29 +0530

    Add app.py

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+from config import Config
+from pymongo import ASCENDING
+
+# Initialize Flask application
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+mongo = PyMongo(app)
+
+with app.app_context():
+    # Create index only once when the app starts
+    mongo.cx.imdb.users.create_index([("username")], unique=True)
+    mongo.cx.imdb.uploads.create_index([("username")])
+    mongo.cx.imdb.movies.create_index([("date_added")])
+    mongo.cx.imdb.movies.create_index([("release_year")])
+    mongo.cx.imdb.movies.create_index([("duration")])
+    mongo.cx.imdb.movies.create_index([("show_id")], unique=True)
+
+    print("Index created")
+
+# Register Blueprints (Organizing routes into modular components)
+from services.auth_service import auth_bp
+from services.upload_service import upload_bp
+from services.movie_service import movie_bp
+
+app.register_blueprint(auth_bp)
+app.register_blueprint(upload_bp)
+app.register_blueprint(movie_bp)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True)
---
+++ config.py
@@ -0,0 +1,13 @@
+import urllib.parse
+
+class Config:
+    # MongoDB settings
+    MONGO_URI = "mongodb+srv://Aryan2000:Aryanag@dailyround.gc7yg.mongodb.net/?retryWrites=true&w=majority&appName=DailyRound"
+    MONGO_DBNAME = "imdb"
+
+    # Flask settings
+    SECRET_KEY = '3a571de28e705d2681f97333394fa322'
+    UPLOAD_FOLDER = './uploads'
+    ALLOWED_EXTENSIONS = {'csv'}
+    # MAX_CONTENT_LENGTH = 10 * 1024 * 1024 * 1024  # Max file size (10 GB)
+    MAX_CONTENT_LENGTH = 1 * 1024 * 1024  # MAx file size 1 MB
